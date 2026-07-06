41c154f6060e33959ec86a310d7a3b59e92a6c92
AlexandrSH-j <kuperlv@mail.ru>
2026-07-06 12:49:54 +0300

    Dopolnil 10 zadani

---
+++ scripts.js
@@ -0,0 +1,57 @@
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+
+let iphoneYear = 2007;
+alert ('Год выпуска первого Iphona: ' + (iphoneYear));
+
+
+let nameСreatorJS = "Брендан Эйх";
+alert ('Имя разработчика JS: ' + (nameСreatorJS));
+
+
+let number1 = 10;
+let number2 = 2;
+alert("Сумма: " + (number1 + number2));
+alert("Разность: " + (number1 - number2));
+alert("Произведение: " + (number1 * number2));
+alert("Частное: " + (number1 / number2));
+
+
+let result = 2 ** 5;
+alert("Результат возведение: " + (result));
+
+
+let numberA = 9;
+let numberB = 2;
+alert("Остаток от деление: " + (numberA % numberB));
+
+
+let num = 1;
+num = num + 5;
+num = num - 3;
+num = num * 7;
+num = num / 3;
+num = num + 1;
+num = num - 1;
+alert(num);
+
+
+let youAge = prompt('Сколько вам лет?');
+alert(youAge);
+
+
+const user = {
+    name: "Александр",
+    age: 28,
+    isAdmin: true
+};
+console.log(user);
+
+
+let youarName = prompt('Какое Ваше имя ?')
+alert('Привет, ' + (youarName) + '!')
+
+
