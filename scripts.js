f463e84c83b9b531ea6a07475b3fd44f764b7cf4
AlexandrSH-j <kuperlv@mail.ru>
2026-07-18 12:18:55 +0300

    hw2 dodel

--- scripts.js
+++ scripts.js
@@ -60,105 +60,105 @@
 
 
 
-let password = '12345';
-let userInput = prompt('Введите пароль');
-if (userInput === password) {
-    alert('Пароль введен верно');
-} else {
-    alert('Пароль введен неправильно');
-}
+// let password = '12345';
+// let userInput = prompt('Введите пароль');
+// if (userInput === password) {
+//     alert('Пароль введен верно');
+// } else {
+//     alert('Пароль введен неправильно');
+// }
 
 
-let c = 2; 
-if (c > 0 && c < 10) {
-    console.log('Верно');
-} else {
-    console.log('Неверно');
-}
+// let c = 2; 
+// if (c > 0 && c < 10) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
 
 
-let d = 120;
-let e = 50;
-if (d > 100 || e > 100) {
-    console.log('Верно');
-} else {
-    console.log('Неверно');
-}
+// let d = 120;
+// let e = 50;
+// if (d > 100 || e > 100) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
 
 
-let a = '2';
-let b = '3';
-alert(+a + +b);
+// let a = '2';
+// let b = '3';
+// alert(+a + +b);
 
 
-let monthNumber = 12;
-if (monthNumber < 1 || monthNumber > 12) {
-    console.log('Ошибка: неверный номер месяца');
-} else {
-    switch (monthNumber) {
-        case 12:
-        case 1:
-        case 2:
-            console.log('Зима');
-            break;
-        case 3:
-        case 4:
-        case 5:
-            console.log('Весна');
-            break;
-        case 6:
-        case 7:
-        case 8:
-            console.log('Лето');
-            break;
-        case 9:
-        case 10:
-        case 11:
-            console.log('Осень');
-            break;
-    }
-}
+// let monthNumber = 12;
+// if (monthNumber < 1 || monthNumber > 12) {
+//     console.log('Ошибка: неверный номер месяца');
+// } else {
+//     switch (monthNumber) {
+//         case 12:
+//         case 1:
+//         case 2:
+//             console.log('Зима');
+//             break;
+//         case 3:
+//         case 4:
+//         case 5:
+//             console.log('Весна');
+//             break;
+//         case 6:
+//         case 7:
+//         case 8:
+//             console.log('Лето');
+//             break;
+//         case 9:
+//         case 10:
+//         case 11:
+//             console.log('Осень');
+//             break;
+//     }
+// }
 
 
 
-let input = prompt('Пожалуйста, введите любое число');
-let number = +input;
-if (isNaN(number) || input === null || input.trim() === '') {
-    alert('Вы ввели не число. Пожалуйста, обновите страницу и попробуйте снова.');
-} else {
-    if (number % 2 === 0) {
-        alert('Число четное');
-    } else {
-        alert('Число нечетное');
-    }
-}
+// let input = prompt('Пожалуйста, введите любое число');
+// let number = +input;
+// if (isNaN(number) || input === null || input.trim() === '') {
+//     alert('Вы ввели не число. Пожалуйста, обновите страницу и попробуйте снова.');
+// } else {
+//     if (number % 2 === 0) {
+//         alert('Число четное');
+//     } else {
+//         alert('Число нечетное');
+//     }
+// }
 
 
-let clientOS = 0; 
-if (clientOS === 0) {
-    console.log('Установите версию приложения для iOS по ссылке');
-} else if (clientOS === 1) {
-    console.log('Установите версию приложения для Android по ссылке');
-} else {
-    console.log('Неизвестная операционная система');
-}
+// let clientOS = 0; 
+// if (clientOS === 0) {
+//     console.log('Установите версию приложения для iOS по ссылке');
+// } else if (clientOS === 1) {
+//     console.log('Установите версию приложения для Android по ссылке');
+// } else {
+//     console.log('Неизвестная операционная система');
+// }
 
 
-let clientOS = 0;
-let clientDeviceYear = 2015; 
-if (clientDeviceYear < 2015) {
-    if (clientOS === 0) {
-        console.log('Установите облегченную версию приложения для iOS по ссылке');
-    } else {
-        console.log('Установите облегченную версию приложения для Android по ссылке');
-    }
-} else {
-    if (clientOS === 0) {
-        console.log('Установите версию приложения для iOS по ссылке');
-    } else {
-        console.log('Установите версию приложения для Android по ссылке');
-    }
-}
+// let clientOS = 0;
+// let clientDeviceYear = 2015; 
+// if (clientDeviceYear < 2015) {
+//     if (clientOS === 0) {
+//         console.log('Установите облегченную версию приложения для iOS по ссылке');
+//     } else {
+//         console.log('Установите облегченную версию приложения для Android по ссылке');
+//     }
+// } else {
+//     if (clientOS === 0) {
+//         console.log('Установите версию приложения для iOS по ссылке');
+//     } else {
+//         console.log('Установите версию приложения для Android по ссылке');
+//     }
+// }
 
 
 
