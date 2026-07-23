5674c53493bd88eec4d4d683673dda1a8f70d053
AlexandrSH-j <kuperlv@mail.ru>
2026-07-23 16:00:46 +0300

    home work - 4 "Циклы"

--- scripts.js
+++ scripts.js
@@ -1,3 +1,59 @@
+for (let i = 0; i < 2; i++) {
+  console.log("Привет");
+}
+
+
+
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+
+
+for (let i = 7; i <= 22; i++) {
+  console.log(i);
+}
+
+
+
+const obj = {
+  "Коля": '200',
+  "Вася": '300',
+  "Петя": '400'
+};
+for (let name in obj) {
+  console.log(`"${name}" — зарплата ${obj[name]} долларов`);
+}
+
+
+
+let n = 1000;
+let num = 0;
+while (n >= 50) {
+    n = n / 2; 
+    num++;
+}
+console.log("Полученное число:", n);
+console.log("Количество итераций:", num);
+
+
+
+
+const firstFriday = 5; 
+for (let day = firstFriday; day <= 31; day += 7) {
+    console.log(`Сегодня пятница, ${day}-е число. Необходимо подготовить отчет.`);
+}
+
+
+
+
+
+
+
+
+
+
+
 // let a = 10;
 // alert(a);
 // a = 20;
