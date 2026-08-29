35fc26df703d70b64e9227a75b467c542a961fb6
AlexandrSH-j <kuperlv@mail.ru>
2026-08-29 11:16:29 +0300

    Kursovoe-Zadanie

--- scripts.js
+++ scripts.js
@@ -113,71 +113,71 @@ function gameKNB() {
 
 // Задания на работу с кодом:
 
-const people1 = [
-   { name: 'Глеб', age: 29 },
-   { name: 'Анна', age: 17 },
-   { name: 'Олег', age: 7 },
-   { name: 'Оксана', age: 47 }
-];
-console.log(people1.sort((a, b) => a.age - b.age));
+// const people1 = [
+//    { name: 'Глеб', age: 29 },
+//    { name: 'Анна', age: 17 },
+//    { name: 'Олег', age: 7 },
+//    { name: 'Оксана', age: 47 }
+// ];
+// console.log(people1.sort((a, b) => a.age - b.age));
 
 
 
-function isPositive(num) {
-  return num > 0;
-}
-function isMale(person) {
-  return person.gender === "male";
-}
-function filter(array, ruleFunction) {
-  const result = [];
-  for (let i = 0; i < array.length; i++) {
-    if (ruleFunction(array[i])) {
-      result.push(array[i]);
-    }
-  }
-  return result;
-}
-console.log(filter([3, -4, 1, 9], isPositive));
-const people2 = [
-  {name: 'Глеб', gender: 'male'},
-  {name: 'Анна', gender: 'female'},
-  {name: 'Олег', gender: 'male'},
-  {name: 'Оксана', gender: 'female'}
-];
-console.log(filter(people2, isMale));
+// function isPositive(num) {
+//   return num > 0;
+// }
+// function isMale(person) {
+//   return person.gender === "male";
+// }
+// function filter(array, ruleFunction) {
+//   const result = [];
+//   for (let i = 0; i < array.length; i++) {
+//     if (ruleFunction(array[i])) {
+//       result.push(array[i]);
+//     }
+//   }
+//   return result;
+// }
+// console.log(filter([3, -4, 1, 9], isPositive));
+// const people2 = [
+//   {name: 'Глеб', gender: 'male'},
+//   {name: 'Анна', gender: 'female'},
+//   {name: 'Олег', gender: 'male'},
+//   {name: 'Оксана', gender: 'female'}
+// ];
+// console.log(filter(people2, isMale));
 
 
 
-const intervalId = setInterval(() => {
-    console.log(new Date());
-}, 3000);
-setTimeout(() => {
-    clearInterval(intervalId);
-    console.log('Время прошло');
-}, 30000);
+// const intervalId = setInterval(() => {
+//     console.log(new Date());
+// }, 3000);
+// setTimeout(() => {
+//     clearInterval(intervalId);
+//     console.log('Время прошло');
+// }, 30000);
 
 
 
-function delayForSecond(callback) {
-  setTimeout(callback, 1000);
-}
-delayForSecond(function () {
-   console.log('Привет, Глеб!');
-})
+// function delayForSecond(callback) {
+//   setTimeout(callback, 1000);
+// }
+// delayForSecond(function () {
+//    console.log('Привет, Глеб!');
+// })
 
 
 
-function delayForSecond(cb) {
-    setTimeout(() => {
-        console.log('Прошла одна секунда');
-        if(cb) {  cb(); }
-    }, 1000)
-}
-function sayHi (name) {
-    console.log(`Привет, ${name}!`);
-}
-delayForSecond(() => sayHi('Глеб'));
+// function delayForSecond(cb) {
+//     setTimeout(() => {
+//         console.log('Прошла одна секунда');
+//         if(cb) {  cb(); }
+//     }, 1000)
+// }
+// function sayHi (name) {
+//     console.log(`Привет, ${name}!`);
+// }
+// delayForSecond(() => sayHi('Глеб'));
 
 
 
