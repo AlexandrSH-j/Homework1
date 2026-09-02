6ae53f8bea4f46e2bb282bbc58d724634d6cbb45
AlexandrSH-j <kuperlv@mail.ru>
2026-09-02 13:15:57 +0300

    KursovoeZadanie2

--- scripts.js
+++ scripts.js
@@ -102,16 +102,12 @@ function gameKNB() {
 
 function gameGRC() {
     const button = document.getElementById('colorBtn');
-    function getRandomColor() {
-      const letters = '0123456789ABCDEF';
-      let color = '#';
-      for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-      }
-      return color;
-    }
     button.addEventListener('click', () => {
-      const randomColor = getRandomColor();
-      document.body.style.background = randomColor;
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    const blocks = document.querySelectorAll(".mini-game-block, .game-block, .hero-page");
+    blocks.forEach(block => {
+        block.style.backgroundColor = randomColor;
     });
+});
 }
+
